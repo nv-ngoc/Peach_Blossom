e50c0ab2375bb0971ddc63d162551832705d0edc
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2019-07-29 18:43:01 +0700

    Work at home

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   enum role: %i[admin user]
 
   validates :username, presence: true, length: { minimum: 6, maximum: 100 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
